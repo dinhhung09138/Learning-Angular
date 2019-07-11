c95280616c61a3845e4cb063d6d02d4ccfaaed88
Hung Tran <hung.tran@tpptechnology.com>
2019-07-11 14:11:20 +0700

    Update select event (error)

--- jquerydatatable/src/app/server-table/server-table.component.ts
+++ jquerydatatable/src/app/server-table/server-table.component.ts
@@ -8,17 +8,51 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ServerTableComponent implements OnInit {
 
+  //dtOptions: DataTables.Settings = {};
   dtOptions: any = {};
+
+  persons: Person[];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+
+    const that = this;
+
     this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      serverSide: true,
       processing: true,
-      //serverSide: true,
-      searching: true,
       ordering: true,
       paging: true,
+      select: true,
+      // responsive: true, // Make page scroll to top.. don't know why
+      ajax: (dataTablesParameters: any, callback) => {
+        that.http
+          .post<DataTablesResponse>(
+            'https://angular-datatables-demo-server.herokuapp.com/',
+            dataTablesParameters, {}
+          ).subscribe(resp => {
+            that.persons = resp.data;
+            console.log(resp.data);
+            console.log(dataTablesParameters);
+            callback({
+              recordsTotal: resp.recordsTotal,
+              recordsFiltered: resp.recordsFiltered,
+              data: []
+            });
+          });
+      },
+      columns: [
+        { title: 'id', data: 'id' },
+        { title: 'first name', data: 'firstName', orderable: false, searchable: false },
+        { title: 'last name', data: 'lastName' }
+      ]
+    };
+
+    /*
+    this.dtOptions = {
       responsive: true,
       //pageLength: 10,
       pagingType: 'full_numbers',
@@ -30,7 +64,7 @@ export class ServerTableComponent implements OnInit {
           orderable: false,
           width: '40px',
           className: 'ctn-center',
-          render: function (obj, type, data, meta) {
+          render(obj, type, data, meta) {
               return meta.row + meta.settings._iDisplayStart + 1;
           }
       },
@@ -44,14 +78,16 @@ export class ServerTableComponent implements OnInit {
       }, {
         title: 'Last name',
         data: 'lastName',
-        class: 'none'
+        class: 'none',
+        orderable: 'false'
       },
       {
           orderable: false,
           width: '70px',
           className: 'ctn-center',
-          render: function (obj, type, data, meta) {
-              var str = '';
+          render(obj, type, data, meta) {
+              const str = '';
+              console.log(data.lastName);
              // str = str + '<a href="/money/mnaccount/history/' + data.ID + '" title="Lịch sử giao dịch" class="mg-lr-2"><i class="fa fa-eye" aria-hidden="true"></i></a>';
              // if (allowEdit === "True") {
              //     str = str + '<a href="/money/mnaccount/edit/' + data.ID + '\" title="Cập nhật tài khoản" title="Cập nhật" class="mg-lr-2"><i class="fa fa-edit" aria-hidden="true"></i></a>';
@@ -63,6 +99,20 @@ export class ServerTableComponent implements OnInit {
           }
       }]
     };
+    */
   }
 
+}
+
+class Person {
+  id: number;
+  firstName: string;
+  lastName: string;
+}
+
+class DataTablesResponse {
+  data: any[];
+  draw: number;
+  recordsFiltered: number;
+  recordsTotal: number;
 }
